d1d43b6feb76aaa51cd8b742dd5337ddccff9af8
Balint Stellek <balint.stellek@gmail.com>
2019-05-06 21:08:55 +0200

    Server sendig message to client infinitely.

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ HOST = '169.254.140.239'    # The remote host
 PORT = 50007              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+    while True:
+        data = s.recv(1024)
+        s.settimeout(0.5)
+        print('Received', repr(data))
--- server.py
+++ server.py
@@ -10,6 +10,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+            conn.send(b'Hello bellooo')
