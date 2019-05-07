d8ccbd9e3b6f0f670fb02e8724dbe536c91a2e76
Balint Stellek <balint.stellek@gmail.com>
2019-05-07 14:35:23 +0200

    Added fake server script.

---
+++ server_fake.py
@@ -0,0 +1,32 @@
+# Echo server program
+import socket
+import random
+# from sense_hat import SenseHat
+# from time import sleep
+
+# sense = SenseHat()
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            # event = sense.stick.wait_for_event()
+            while True:
+                p = random.randint(1, 5)
+                if p == 1:
+                    event = "up"
+                else if p = 2:
+                    event = "down"
+                else if p = 3:
+                    event = "left"
+                else if p = 4:
+                    event = "right"
+                else:
+                    event = "middle"
+                conn.send(event.direction.encode())
+                sleep(5)
