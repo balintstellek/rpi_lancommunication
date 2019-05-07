3945c2f7c67253ac9cb7962bf38de9d57984599f
Balint Stellek <balint.stellek@gmail.com>
2019-05-07 14:50:17 +0200

    Fixed some problems with fake server script.

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 import socket
 
 HOST = '169.254.140.239'    # The remote host
-PORT = 50007              # The same port as used by the server
+PORT = 50002              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
