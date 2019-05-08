c4a4d2703b2671d64d1d79254b7fa63ed6c8d422
Balint Stellek <balint.stellek@gmail.com>
2019-05-08 15:06:54 +0200

    Added buttons to GUI.

--- client_fake.py
+++ client_fake.py
@@ -2,6 +2,20 @@ import random
 import time
 from tkinter import *
 
+def close_window ():
+    root.destroy()
+
+def socket_comm():
+    directions = ["up", "down", "left", "right", "middle"]
+    while True:
+            event = random.choice(directions)
+            time.sleep(2)
+            listbox.insert(END,event)
+            listbox.pack()
+            root.update_idletasks()
+            root.update()
+            print(event)
+
 root = Tk()
 root.minsize(200,300)
 label = Label(root,text='Joystick GUI')
@@ -11,17 +25,10 @@ root.config(bg='gray')
 listbox = Listbox(root, bg = "gray")
 listbox.pack()
 
+startbutton = Button(root, bg = "blue", text = 'Start',  width=15, height=1, command = socket_comm)
+startbutton.pack(side = LEFT)
 
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+stopbutton = Button(root, bg = "blue", text = 'Quit',  width=15, height=1, command = close_window)
+stopbutton.pack(side = RIGHT)
 
-directions = ["up", "down", "left", "right", "middle"]
-while True:
-            event = random.choice(directions)
-            time.sleep(2)
-            listbox.insert(END,event)
-            listbox.pack()
-            root.update_idletasks()
-            root.update()
-            print(event)
 root.mainloop()
